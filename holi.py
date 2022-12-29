2be40ddcfc52076cd91e4af559398de01ab425c7
iespinozah <isabel.espinozah@gmail.com>
2022-12-29 02:24:49 -0300

    Update holi.py

--- holi.py
+++ holi.py
@@ -1,1 +1,11 @@
 print ("hola")
+print ('esto es una prueba')
+nombre = "Ruth Isabel"
+apellido= "Espinoza Herrera"
+edad = 35
+print ("Hola soy " + nombre + " " + apellido + " y tengo " + str(edad) + " años")
+fav = 9
+print ("numero favorito: " + str(fav))
+num1 = 10
+num2 = 6
+result= num1*num2
