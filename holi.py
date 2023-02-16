bab95a6bd54540e157384ad5268ba148aa4d6c90
iespinozah <isabel.espinozah@gmail.com>
2023-02-16 17:29:39 -0300

    Update holi.py

--- holi.py
+++ holi.py
@@ -1,3 +1,15 @@
+print ("hola")
+print ('esto es una prueba')
+nombre = "Ruth Isabel"
+apellido= "Espinoza Herrera"
+edad = 35
+print ("Hola soy " + nombre + " " + apellido + " y tengo " + str(edad) + " años")
+fav = 9
+print ("numero favorito: " + str(fav))
+num1 = 10
+num2 = 6
+result= num1*num2
+print ("el resultado de " , num1,  " por ", num2,  " es " , result)
 
 
 def invertir():
