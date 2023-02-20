115f6c01cd486b4155acf676d51f165539900076
iespinozah <isabel.espinozah@gmail.com>
2023-02-19 23:02:16 -0300

    final

--- formulas.py
+++ formulas.py
@@ -3,10 +3,4 @@ def area(x, y):
     peri=2*(x+y)
     return ar, peri
 
-x=int(input("Escriba la base: "))
-y=int(input("Escriba la altura: "))
 
-a, p = area(x, y)
-
-print ("Area ", a)
-print("perimetro ", p)
--- holi.py
+++ holi.py
@@ -4,11 +4,11 @@ import formulas
 def main():
     x=int(input("Escriba la base: "))
     y=int(input("Escriba la altura: "))
-    a=formulas.area(x,y)
-    print (formulas.area(x, y))
 
-    print("El área es: ", ar)
-    print("El perímetro es: ", peri)
+    a, p = formulas.area(x, y)
+
+    print ("Area ", a)
+    print("perimetro ", p)
 
 main()
 
